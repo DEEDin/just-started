f61d7e4e1e76232763ec47f7306b812713e1a316
DEEDin <48057278+DEEDin@users.noreply.github.com>
2019-04-29 13:16:39 +0545

    Add files via upload

---
+++ new.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c,z;
+   printf("Enter the first number a=");
+   scanf("%d",&a);
+   printf("Enter the second number b=");
+   scanf("%d",&b);
+   printf("Enter the third number c=");
+   scanf("%d",&c);
+   z=a*b+b*c+a*b*c;
+   printf("THe equation of z=a*b+b*c+a*b*c is %d",z);
+
+
+
+}
